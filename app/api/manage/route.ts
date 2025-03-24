300451d0675123cd19d740540a8f04cf9d6bc1e6
Stephen Parker <stephenwsws@gmail.com>
2025-03-24 17:37:29 -0500

    feat(demo): go to manage page

--- app/api/manage/route.ts
+++ app/api/manage/route.ts
@@ -1,17 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
-import type { User } from '@workos-inc/node'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export async function POST(request: NextRequest) {
-  const { user }: { user: User } = await request.json()
-  console.log('user', user)
+  const { customerId }: { customerId: string } = await request.json()
 
   try {
-    // Get the customer ID from your database based on the authenticated user
-    const customerId = 'cus_example123' // Replace with actual customer ID retrieval logic
-
     const session = await stripe.billingPortal.sessions.create({
       customer: customerId,
       return_url: `${request.headers.get('origin')}/account`,
--- components/account.tsx
+++ components/account.tsx
@@ -20,11 +20,14 @@ type Plan = {
   price_id: string
 }
 
+type Customer = {
+  id: string
+  email: string
+}
+
 export const Account = ({ user }: { user: User }) => {
   {
-    const [customer, setCustomer] = useState<Record<string, unknown> | null>(
-      null,
-    )
+    const [customer, setCustomer] = useState<Customer>()
     const [plans, setPlans] = useState<Plan[]>([])
     const searchParams = useSearchParams()
     const canceled = searchParams.get('canceled')
@@ -35,7 +38,7 @@ export const Account = ({ user }: { user: User }) => {
       try {
         const response = await fetch('/api/manage', {
           method: 'POST',
-          body: JSON.stringify({ user }),
+          body: JSON.stringify({ customerId: customer?.id }),
         })
         const { url } = await response.json()
         window.location.href = url
@@ -60,7 +63,11 @@ export const Account = ({ user }: { user: User }) => {
         body: JSON.stringify({ email: user.email }),
       })
         .then((res) => res.json())
-        .then((data) => setCustomer(data))
+        .then(({ customer }) => {
+          if (customer.data.length > 0) {
+            setCustomer(customer.data[0])
+          }
+        })
     }, [])
 
     const handleSubscribe = async (priceId: string) => {
@@ -86,23 +93,27 @@ export const Account = ({ user }: { user: User }) => {
         <h1 className="font-extrabold text-3xl tracking-tight my-8 uppercase">
           My Account
         </h1>
-        <div className="flex gap-2">
-          <Image
-            src={user?.profilePictureUrl!}
-            alt="Profile picture"
-            width={50}
-            height={50}
-            className="rounded-full"
-          />
+        <div className="flex gap-2 w-full">
+          <div>
+            <Image
+              src={user?.profilePictureUrl!}
+              alt="Profile picture"
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+          </div>
           <div>
             <p className="font-bold text-xl">
               {user?.firstName} {user?.lastName}
             </p>
             <p className="text-stone-500">{user?.email}</p>
+            {customer && (
+              <p className="text-stone-500">Stripe ID: {customer?.id}</p>
+            )}
           </div>
         </div>
         <div className="flex flex-col gap-4 my-8">
-          <pre>{JSON.stringify(customer, null, 2)}</pre>
           <h2 className="font-bold text-2xl mb-4">
             Choose a Subscription Plan
           </h2>
@@ -126,15 +137,17 @@ export const Account = ({ user }: { user: User }) => {
               </div>
             ))}
           </div>
-          <div className="mt-8">
-            <button
-              onClick={handleManageSubscription}
-              disabled={isLoading}
-              className="rounded-lg bg-red-600 text-white py-2 px-4 hover:cursor-pointer"
-            >
-              {isLoading ? 'Loading...' : 'Manage Subscription'}
-            </button>
-          </div>
+          {customer?.id && (
+            <div className="mt-8">
+              <button
+                onClick={handleManageSubscription}
+                disabled={isLoading}
+                className="rounded-lg bg-red-600 text-white py-2 px-4 hover:cursor-pointer"
+              >
+                {isLoading ? 'Loading...' : 'Manage Subscription'}
+              </button>
+            </div>
+          )}
           {canceled && <p className="text-red-500">Payment canceled.</p>}
         </div>
       </div>
